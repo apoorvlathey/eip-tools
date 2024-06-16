0ef12423a14bc43711c44965fe701ccc789d8e6b
apoorvlathey <latheyapoorv@gmail.com>
2024-06-17 00:38:36 +0400

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Providers } from "./providers";
 import { getMetadata } from "@/utils";
+import { Analytics } from "@/components/Analytics";
 
 const poppins = Poppins({ weight: ["200", "400", "700"], subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Analytics />
         <Providers>{children}</Providers>
       </body>
     </html>
